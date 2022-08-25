9f1de0dbec8561edafd00ebecd87dea26df0ede9
mvn-quangnguyen-hn <quang.nguyen@monstar-lab.com>
2022-08-25 10:42:00 +0700

    test flow

---
+++ provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.33.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_project_service" "project" {
+  service = "compute.googleapis.com"
+}
