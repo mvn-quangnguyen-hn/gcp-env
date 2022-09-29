1330ad850bdd3d9b4afc6683afbe8b2fe05fe4f4
mvn-quangnguyen-hn <quang.nguyen@monstar-lab.com>
2022-09-29 15:04:18 +0700

    update vpc

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.33.0"
+    }
+  }
+  backend "gcs" {
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
